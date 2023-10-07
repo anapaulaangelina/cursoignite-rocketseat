1865d7a5aadbf52bdfda9fcf7f87437e622ecc7d
Ana Paula Angelina da Rocha <anapaula.angelina@yahoo.com.br>
2023-10-07 20:23:55 -0300

    P1 M1 A6 - Propriedades

--- trilha-reactjs/projeto-01/src/App.jsx
+++ trilha-reactjs/projeto-01/src/App.jsx
@@ -3,7 +3,10 @@ import { Post } from "./Post";
 export function App() {
   return (
     <div>
-      <Post />
+      <Post
+        author="Diego Fernandes"
+        content="L Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae hic aliquam, sed deserunt molestiae, aliquid consequatur omnis harum ipsam iste neque quidem debitis dignissimos fugiat iure repellendus nisi rerum optio. "
+      />
       <Post />
       <Post />
       <Post />
